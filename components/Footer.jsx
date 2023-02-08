143f4bae100c1d39fc02b89319d94a33369fe083
alinaincodeland <alinacuznetov@gmail.com>
2023-02-08 18:14:24 +0100

    Added changes to footer component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="bg-light p-3 text-center" data-testid="footer">
-    <div className="logo" data-testid="footer-logo" />
-    {/* <p data-testid="footer-text">
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
-    </p> */}
+  <footer className="bg-light pt-5 site-footer" data-testid="footer">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12 col-md-6">
+          <h6>DCI Connect</h6>
+          <p className="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium excepturi eos, esse fugit dolorem odio
+            id! Repellat ullam quaerat laborum. Made by Alina and Oliveira.
+          </p>
+        </div>
+
+        <div className="col-xs-6 col-md-3">
+          <h6 className="text-center">Support</h6>
+          <ul className="footer-links">
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-xs-6 col-md-3">
+          <h6 className="text-center">Legal</h6>
+          <ul className="footer-links">
+            <li>
+              <a href="#">Imprint</a>
+            </li>
+            <li>
+              <a href="#">Privacy</a>
+            </li>
+            <li>
+              <a href="#">Cookies</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </footer>
 );
 
