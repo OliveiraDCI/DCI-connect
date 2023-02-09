627ad95313e8b6271a39f87015ef0767791fc7fc
OliveiraDCI <oliveira.sein.dci@gmail.com>
2023-02-09 09:18:09 +0100

    Home Page styles adjustments.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,10 +5,10 @@ import team from "../public/team.gif";
 const Hero = () => (
   <div className="hero mt-3 text-center d-flex" data-testid="hero">
     <div className="hero-text-container">
-      <h1 className="hero hero-title mb-3" data-testid="hero-title">
-        WELCOME TO <br /> DCI CONNECT
+      <h1 className="hero-title" data-testid="hero-title">
+        WELCOME TO DCI CONNECT
       </h1>
-      <p className="lead mx-0 my-3" data-testid="hero-lead">
+      <p className="hero-para mx-0 my-3" data-testid="hero-lead">
         Our platform is all about bringing current DCI students and alumni together for some seriously awesome
         mentorship opportunities.
       </p>
